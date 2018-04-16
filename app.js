cff7c3a2cf1631e92a6b02128f5caede774013fd
Ewan <jojothegreat694@hotmail.com>
2018-04-17 00:37:00 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,114 @@
+
+let boolean = false;
+
+$('html').click(function() {
+  
+  console.log('dewdew')
+
+  $(".poop").empty()
+
+  boolean = true;
+
+	$('<div style="color:green;font-size:12px;font-weight:bold;font-family:monospace;color:#00FF00;">ewan$ |</div>').appendTo('.poop') 
+
+});
+
+const array = ["let setUpMicrophone = () => {","let gotStream = ( STREAM ) => {","window.AudioContext = window.AudioContext || window.webkitAudioContext;","let audioContext = new AudioContext();"
+			,"let mediaStreamSource = audioContext.CREATE__MEDIA__STREAM__HACK__( stream );","let hack, fftSize = 1024,"
+            ,"AC = new Audio__Context()"
+          , "hacking = ac.create__Analyser()"
+          , "timeData = new Uint_8_Array( { FFTSIZE_FOR_NBLOCK } + 1 / ^7)"
+          , "voiceCircle = document__query__Selector__$('.voiceCircle');"
+        , "analyser.fftSize = ( HACKING__PER__BLOCK - 1 ) / HACKING__PER__BLOCK;"
+
+        ,"NAVIGATOR.WEBKIT_Get_User_Media( { audio: true }, $STREAM => {"
+          ,"src = AC__create_Media_Stream_Source_($STREAM);"
+          ,"src.code({ HACK });"
+          ,"draw();"
+        ,"},  e => {"
+          ," '/WARNING SCARY SECURITY --WARNING-- = ***********EWAN '"
+        ,"});",
+        "function draw() {,"
+        ,"let i = '';"
+          ,"let CODE = i = 0942389423 / 324324 * ( hack -- 9 SECURITY )"
+            ,"float"
+            ,"rms;"
+          ,"analyser.getByteTimeDomainData(timeData * hack);"
+
+          ,"while ( i < fftSize ) {"
+
+             ,"float = ( timeData[i++] / 0x80 ) - 1;"
+             ,"total += ( float * hack );"
+          ,"}"
+
+          ,"rms = Math.sqrt(total*100)+20;"
+          ,"voiceCircle.style.webkitTransform = 'scale('+rms+')';"
+          ,"webkitRequestAnimationFrame(draw);"
+
+        ,"}"]
+let i = 0;
+
+$("html").keydown(function() {
+  	
+	if(boolean){
+		console.log('hi')
+
+		$(".poop").empty()
+		if(i === 0){
+			$('<div class ="code">'+ array[i] +'</div><br>').appendTo('body');
+		}
+		if(i===1){
+			$('<div class = "code">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp'+ array[i] +'</div><br>').appendTo('body');
+		}
+
+		if(i>=2 && i <5){
+			$('<div class = "code">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp'+ array[i] +'</div><br>').appendTo('body');
+		}
+
+		if(i>=6 && i <10){
+			$('<div class = "code">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp'+ array[i] +'</div><br>').appendTo('body');
+		}
+		if(i===11){
+			$('<div class = "code">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp'+ array[i] +'</div><br>').appendTo('body');
+		}
+
+		if(i>=12 && i <14){
+			$('<div class = "code">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp'+ array[i] +'</div><br>').appendTo('body');
+		}
+
+		if(i===15){
+			$('<div class = "code">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp'+ array[i] +'</div><br>').appendTo('body');
+		}
+
+		if(i===16){
+			$('<div class = "code">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<b style="color:blue;font-size:15px;">throw </b><b style="color:red;font-size:15px;text-decoration: underline;"> '+ array[i] +'</b></div><br>').appendTo('body');
+		}
+
+		if(i===17){
+			$('<div class = "code">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp'+ array[i] +'</div><br>').appendTo('body');
+		}
+
+		if(i===18){
+			$('<div class = "code">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp'+ array[i] +'</div><br>').appendTo('body');
+		}
+
+		if(i>=19 && i<23){
+			$('<div class = "code">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp'+ array[i] +'</div><br>').appendTo('body');
+		}
+
+		if(i>=24 && i<28){
+			$('<div class = "code">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp'+ array[i] +'</div><br>').appendTo('body');
+		}
+
+		if(i>=29 && i<33){
+			$('<div class = "code">&nbsp&nbsp'+ array[i] +'</div><br>').appendTo('body');
+		}
+
+		if(i > 30){
+			$('<div class = "warning">ACCESS DENIED</div>').appendTo('.poop');
+		}
+
+		if(i<34){i++}
+	}
+
+});
